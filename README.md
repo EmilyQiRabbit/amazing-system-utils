c8bb614be4b9749bb2b68fff6e1ed3f4c23463d6
EmilyQiRabbit <LiuYQEmily@163.com>
2021-01-01 20:42:24 +0800

    feat: init longtask monitor

--- README.md
+++ README.md
@@ -27,3 +27,5 @@
 ### Self
 
 1. ~~[autoRefetch](https://github.com/EmilyQiRabbit/amazing-system-utils/blob/master/autoRefetch/autoRefetch.ts): 远端请求失败时，自动在 window.requestIdleCallback() 中重新拉取。~~ （疯了吧自己造轮子 🤪，有现成又好用的它不香吗？！参见 [p-retry](https://github.com/sindresorhus/p-retry)，呵呵）
+
+2. Longtask Monitor: 基于 PerformanceObserver 和 eventemitter3 的长任务监听工具。
---
+++ longtaskMonitor/index.ts
@@ -0,0 +1,77 @@
+// 无依赖，临时版本，仅代码同步
+import EventEmitter from 'eventemitter3';
+
+type LoggerParams = {
+    name: string;
+    duration: number;
+    longtaskCount: number;
+    longtaskSum: number;
+    originData: any;
+}
+
+type LoggerFunc = (params: LoggerParams) => void;
+
+const defaultLoggerFunc = (params: LoggerParams) => {
+    console.log('long task monitor log:', params);
+}
+
+export default class LongTaskObserver extends EventEmitter {
+    private observer: PerformanceObserver;
+    private reporter: LoggerFunc;
+    // private debugFunc: LoggerFunc;
+    private eventName: string = 'longtask';
+    private records: PerformanceEntryList = [];
+
+    public isLongTasksSupported: boolean = this._isLongTasksSupported();
+
+    private _isLongTasksSupported() {
+        return 'PerformanceObserver' in window
+            && 'PerformanceLongTaskTiming' in window
+            && 'TaskAttributionTiming' in window
+    }
+
+    constructor(initParams: {reporter: LoggerFunc} = {reporter: defaultLoggerFunc}) {
+        super();
+        const { reporter } = initParams;
+        this.reporter = reporter;
+        this.observer = PerformanceObserver ? new PerformanceObserver(this.emitter) : null;
+        this.observer.observe({entryTypes: ["longtask"]});
+        return this;
+    }
+
+    private emitter: PerformanceObserverCallback = (list) => {
+        const perfEntries = list.getEntries();
+        // 广播长任务信息
+        this.emit(this.eventName, perfEntries);
+    }
+
+    start = () => { // 业务封装
+        this.records = [];
+        const startTime = Date.now();
+        const recordPerf = (perfEntries: PerformanceEntryList) => {
+            this.records.push(...perfEntries);
+        };
+        this.on(this.eventName, recordPerf);
+        const stop = (params?: {noReport: boolean}) => {
+            this.removeListener(this.eventName, recordPerf);
+            if (this.records.length && !params?.noReport) {
+                let reportData = {
+                    name: this.eventName,
+                    duration: Date.now() - startTime,
+                    longtaskCount: this.records.length,
+                    originData: this.records,
+                    longtaskSum: this.records.map(record => record.duration).reduce((acc, cur) => acc + cur)
+                }
+                this.reporter(reportData);
+                this.records = [];
+            }
+        }
+        return stop;
+    }
+
+    destroy = () => {
+        this.observer && this.observer.disconnect();
+        this.observer = null;
+        this.records = [];
+    }
+}
